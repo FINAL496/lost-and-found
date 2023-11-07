4dee28d216aa36f4d996e6bcbb7f4e4d025d4f2a
FINAL496 <150138401+FINAL496@users.noreply.github.com>
2023-11-07 17:08:12 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $lostOrFound = $_POST["lost_or_found"];
+    $rrnNo = $_POST["number"];
+    $name = $_POST["name"];
+    $contact = $_POST["contact"];
+    $description = $_POST["description"];
+    
+    // Notify the operator (change this to the operator's email address)
+    $to = "operator@example.com"; // Replace with the operator's email address
+    $subject = "Lost and Found Report";
+    $message = "A new report has been submitted for verification:\n\n"
+             . "Lost/Found: $lostOrFound\n"
+             . "RRN No: $rrnNo\n"
+             . "Name: $name\n"
+             . "Contact: $contact\n"
+             . "Description: $description";
+    $headers = "From: webmaster@example.com"; // Replace with your website's email address
+
+    // Send the email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Response recorded. The operator has been notified for verification. Thank you!";
+    } else {
+        echo "Failed to send the email. Please try again later.";
+    }
+}
+?>
